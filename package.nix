1cb7c2ca5f3ae921934d6f23f57169fc6778c1c3
sadjow <177525+sadjow@users.noreply.github.com>
2026-03-16 21:18:50 +0000

    chore: update gemini-cli to version 0.33.2

--- package.nix
+++ package.nix
@@ -13,11 +13,11 @@
 }:
 
 let
-  version = "0.33.1";
+  version = "0.33.2";
 
   geminiBundled = fetchurl {
     url = "https://github.com/google-gemini/gemini-cli/releases/download/v${version}/gemini.js";
-    hash = "sha256-AWsXfHOfd5HoseqacmfkO+vuJStuG2vLysd0EUyF2f8=";
+    hash = "sha256-mSFsftSSn3vwfiTWnJw0unpxIsxUMfMU5ZM1WWXMJmI=";
   };
 in
 stdenv.mkDerivation {
